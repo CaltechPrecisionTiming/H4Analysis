90c79de7ba1f284b9e522df856088d77a6bf62de
Zhicai <zzhang2@caltech.edu>
2016-11-09 20:39:11 +0100

    update input path

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -1,1 +1,1 @@
-python submitBatch.py -r=list_HGC_Nov.list -s=root://eoscms//eos/cms//store/caf/user/zhicaiz/H4_Nov2016/ -v=08Nov2016_v1 -c=../cfg/H4_t1065_test.cfg -q=1nh
+python submitBatch.py -r=list_HGC_Nov.list -s=root://eoscms//eos/cms/store/group/phys_susy/razor/Timing/Nov2016CERN/ -v=v1 -c=../cfg/H4_t1065_test.cfg -q=1nh
